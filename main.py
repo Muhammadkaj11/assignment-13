41166af7d8713da26f9ac5b44cff3c963bf6d6b2
Muhammadkaj11 <muhdkaj11@gmail.com>
2024-10-27 21:29:19 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def decimal_to_binary(decimal_number):
+    if decimal_number == 0:
+        return "0"
+    binary_number = ""
+    while decimal_number > 0:
+        remainder = decimal_number % 2
+        binary_number = str(remainder) + binary_number
+        decimal_number = decimal_number // 2
+    return binary_number
+
+# Example usage
+decimal_number = int(input("Enter a decimal number: "))
+print(f"Binary representation: {decimal_to_binary(decimal_number)}")
+
+
+    
